f42b7a6fb3ee8b0f7467832ad25fd0207e8240ba
Alexandre Harel <mcfly@mywebsoluce.fr>
2023-11-15 21:10:18 +0100

    V 0.3.8 Fix perm sed

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ echo -e "\n\n Mise à jour de la configuration de PHPMyadmin \n"
 config_file="/etc/phpmyadmin/config.inc.php"
 search="// \$cfg\['Servers'\]\[\$i\]\['AllowNoPassword'\] = TRUE;"
 replace="\$cfg['Servers'][\$i]['AllowNoPassword'] = TRUE;"
-sed -i "s#$search#$replace#" $config_file &&
+sudo sed -i "s#$search#$replace#" $config_file &&
 echo "Include /etc/phpmyadmin/apache.conf" | sudo tee -a /etc/apache2/apache2.conf
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
 
